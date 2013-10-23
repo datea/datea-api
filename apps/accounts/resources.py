51ba6cf995372f0e23e1326b17de2dddd4e7a9ee
Javier Olaechea <pirata@gmail.com>
2013-10-23 17:20:14 -0500

    Add UserCreateResource

--- apps/accounts/resources.py
+++ apps/accounts/resources.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 from tastypie.resources import ModelResource
+from tastypie.authentication import BasicAuthentication, Authentication
+# -*- coding: utf-8 -*-
+from tastypie import fields
+from tastypie.authorization import Authorization
+from tastypie.serializers import Serializer
 
 from .models import User
 
@@ -7,3 +12,24 @@ from .models import User
 class UserResource(ModelResource):
     class Meta:
         queryset = User.objects.all()
+        allowed_methods = ['put', 'get', 'patch', ]
+        excludes = ['is_superuser', 'is_staff', 'is_active', 'password', ]
+        authentication = Authentication()
+        authorization = Authorization()
+
+
+
+# Adapted from From: http://psjinx.com/programming/2013/06/07/so-you-want-to-create-users-using-djangotastypie/
+class CreateUserResource(ModelResource):
+    class Meta:
+        queryset = User.objects.all()
+        excludes = ['is_superuser', 'is_staff', 'is_active', 'last_login',
+                    'id', ]
+        allowed_methods = ['post']
+        always_return_data = True
+        authentication = Authentication()
+        authorization = Authorization()
+        serializer = Serializer()
+
+        resource_name = 'create_user'
+        always_return_data = True
--- apps/api/urls.py
+++ apps/api/urls.py
@@ -2,12 +2,13 @@
 from django.conf.urls import patterns, include, url
 
 from tastypie.api import Api
-from accounts.resources import UserResource
+from accounts.resources import UserResource, CreateUserResource
 from datum.resources import DatumResource
 
 
 v2_api = Api(api_name='v2')
 v2_api.register(UserResource())
+v2_api.register(CreateUserResource())
 v2_api.register(DatumResource())
 
 urlpatterns = patterns('',

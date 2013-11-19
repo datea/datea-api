0624ceca575e2404b5e29247a85e29685cff5ec4
Javier Olaechea <pirata@gmail.com>
2013-11-19 15:10:28 -0500

    Redirect the top level to the API's entry point

--- urls.py
+++ urls.py
@@ -1,8 +1,18 @@
 # -*- coding: utf-8 -*-
+from django.shortcuts import redirect
+
+from django.core.urlresolvers import reverse
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 admin.autodiscover()
+
+
+def top_level(request):
+    """Redirect to the API's entry point.
+
+    """
+    return redirect(reverse('api_v2_top_level', kwargs={'api_name': 'v2'}))
 
 urlpatterns = patterns(
     '',
@@ -14,4 +24,5 @@ urlpatterns = patterns(
     url(r'^django-rq/', include('django_rq.urls')),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', top_level),
 )

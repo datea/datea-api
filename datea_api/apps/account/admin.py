4ad72376c5e43e024225b250944fe0d25e06d6ec
http://rdrg@github.com <rodrigoderteano@gmail.com>
2014-01-27 08:22:54 -0500

    better user admin

--- datea_api/apps/account/admin.py
+++ datea_api/apps/account/admin.py
@@ -21,8 +21,8 @@ class CustomUserAdmin(UserAdmin):
     change_user_password_template = None
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
-        (_('Personal info'), {'fields': ('full_name', 'email', 'message')}),
-        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+        (_('Personal info'), {'fields': ('full_name', 'email', 'message', 'image', 'bg_image')}),
+        (_('Permissions'), {'fields': ('status', 'is_active', 'is_staff', 'is_superuser',
                                        'groups', 'user_permissions')}),
         (_('Important dates'), {'fields': ('last_login',)}),
     )
@@ -37,8 +37,9 @@ class CustomUserAdmin(UserAdmin):
     list_display = ('username', 'email', 'full_name', 'is_staff')
     list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
     search_fields = ('username', 'full_name', 'email')
-    ordering = ('username','created','last_login')
+    ordering = ('-created','username','email', 'full_name', 'last_login')
     filter_horizontal = ('groups', 'user_permissions',)
+    list_display = ('username', 'full_name', 'email', 'created', 'last_login')
 
     inlines = UserAdmin.inlines + [ApiKeyInline]
 

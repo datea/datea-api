42fcf72e3ac65c90c83296dc25ae1f9776f4753f
http://rdrg@github.com <rodrigoderteano@gmail.com>
2014-02-18 00:25:13 -0500

    account image upload debug 10

--- datea_api/apps/campaign/resources.py
+++ datea_api/apps/campaign/resources.py
@@ -65,7 +65,7 @@ class CampaignResource(DateaBaseGeoResource):
 
 
         if 'image' in bundle.data and type(bundle.data['image']) == DictType and 'image' in bundle.data['image']:
-                
+            print bundle.data['image']
             if 'id' in bundle.data['image'] and 'data_uri' not in bundle.data['image']['image']:
                 bundle.obj.image_id = postData['image']['id']
             else:

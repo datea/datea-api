2a4a80550621dbdac9094a5de607780ba7c86334
http://rdrg@github.com <rodrigoderteano@gmail.com>
2014-07-11 21:16:41 -0300

    m2m index debug v7

--- datea_api/apps/api/signals.py
+++ datea_api/apps/api/signals.py
@@ -1,11 +1,4 @@
-from django.db.models import signals
 from django.dispatch import Signal
-from haystack.signals import BaseSignalProcessor
-
-from datea_api.apps.dateo.models import Dateo
-from datea_api.apps.tag.models import Tag
-from datea_api.apps.campaign.models import Campaign
-from datea_api.apps.notify.models import ActivityLog
 
 # RESOURCE SAVED SIGNAL
 resource_saved = Signal(providing_args=['instance', 'created'])

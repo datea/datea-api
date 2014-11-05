3413494b3598e1a05ee5584c05f61e50c52e54c0
http://rdrg@github.com <rodrigoderteano@gmail.com>
2014-11-04 22:17:57 -0500

    indentation

--- datea_api/apps/campaign/admin.py
+++ datea_api/apps/campaign/admin.py
@@ -2,9 +2,8 @@ from django.contrib import admin
 from .models import Campaign
 
 class CampaignAdmin(admin.ModelAdmin):
-    model = Campaign
-    
-    search_fields = ['name', 'main_tag__tag']
-    readonly_fields = ('dateo_count', 'comment_count', 'follow_count')
+	model = Campaign
+	search_fields = ['name', 'main_tag__tag']
+	readonly_fields = ('dateo_count', 'comment_count', 'follow_count')
 
 admin.site.register(Campaign, CampaignAdmin)
